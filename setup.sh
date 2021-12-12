e45a3a8ff9278c42441e1df69bdd26d4867764f8
tw_yshuang <tw.yshuang@gmail.com>
2021-12-12 14:50:24 +0800

    [add] setup.sh, logout.script.

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+case $SHELL in
+    *zsh )
+    profile=~/.zshrc
+    logout_profile=~/.zlogout
+    ;;
+    *bash )
+    profile=~/.bashrc
+    logout_profile=~/.bash_logout
+    ;;
+    * )
+    Echo_Color r "Unknow shell, need to manually add pyenv config on your shell profile!!"
+    ;;
+esac
+
+cp ./git-acc.sh ~/.git-acc
+printf "" >> ~/.gitacc
+printf "\n# git account switch\nsource ~/.git-acc\n" >> $profile
+echo "$(cat ./logout.script)" >> $logout_profile
+
+echo "Done!! Please logout"
