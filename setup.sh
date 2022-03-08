11cdebc25e59423d00117c1892bca022f16d50f0
tw-yshuang <tw.yshuang@gmail.com>
2022-03-08 15:22:21 +0800

    [fix] setup.sh, fix update multiple write code error.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
+function Echo_Color(){
+    case $1 in
+        r* | R* )
+        COLOR='\e[31m'
+        ;;
+        g* | G* )
+        COLOR='\e[32m'
+        ;;
+        y* | Y* )
+        COLOR='\e[33m'
+        ;;
+        b* | B* )
+        COLOR='\e[34m'
+        ;;
+        *)
+        echo "$COLOR Wrong COLOR keyword!\e[0m" 
+        ;;
+        esac
+        echo -e "$COLOR$2\e[0m"
+    }
 
 case $SHELL in
     *zsh )
@@ -14,7 +34,8 @@ case $SHELL in
     ;;
 esac
 
-gitacc_config="\n# git account switch\nsource ~/.git-acc\n"
+gitacc_config='# git account switch
+source $HOME/.git-acc'
 
 cp ./git-acc.sh ~/.git-acc
 
@@ -22,7 +43,7 @@ if [ "$(grep -xn "$gitacc_config" $profile)" != "" ]; then
     Echo_Color g "You have already added git-acc config in $profile !!\nOnly update your git-acc!"
 else
     printf "" >> ~/.gitacc
-    echo $gitacc_config >> $profile
+    printf "$gitacc_config\n" >> $profile
     echo "$(cat ./logout.script)" >> $logout_profile
 fi
 
