920a7d51412b26c50f8ab0bf131a8f6b94a186b8
tw-yshuang <tw.yshuang@gmail.com>
2026-02-12 00:01:32 +0800

    [mod] git-acc.sh, filter [DEFAULT] from account enumeration to prevent it appearing in tab completion or account lists.

--- git-acc.sh
+++ git-acc.sh
@@ -66,7 +66,7 @@ function git-acc(){
   }
 
   local function _acc(){
-    local users_info=$(cat "$gitacc_locate" | grep -n '\[.*\]')
+    local users_info=$(cat "$gitacc_locate" | grep -n '\[.*\]' | grep -v '\[DEFAULT\]')
     accs_line=($(echo $users_info | cut -f1 -d ':'))
     accnames=($(echo $users_info | cut -d '[' -f2 | cut -d ']' -f1))
     unset users_info
@@ -226,7 +226,7 @@ function git-acc(){
 
 local function _git-acc(){
   local function _acc(){
-    local users_info=$(cat $HOME/.gitacc | grep -n '\[.*\]')
+    local users_info=$(cat $HOME/.gitacc | grep -n '\[.*\]' | grep -v '\[DEFAULT\]')
     local accs_line=$(echo $users_info | cut -f1 -d ':')
     local accnames=$(echo $users_info | cut -d '[' -f2 | cut -d ']' -f1)
     echo "${accnames[*]}" | tr ' ' '\n'
